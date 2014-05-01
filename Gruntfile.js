b012433366529cb495b38e9a8ffe240ff0d19996
steven.jacobs.post+gmail@gmail.com <steven.jacobs.post+github@gmail.com>
2014-05-01 19:40:17 -0400

    added sass, and droid serif from googlefonts

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,53 +1,69 @@
 module.exports = function(grunt) {
-        grunt.initConfig({
-                responsive_images: {
-                        myTask: {
-                                options: {
-                                        sizes: [{
-                                                name: "small",
-                                                width: 160,
-                                                height: 120
-                                        },{
-                                                name: 'medium',
-                                                width: 320,
-                                                height: 240
-                                        },{
-                                                name: 'large',
-                                                width: 640,
-                                                height: 480
-                                        }]
-                                },
-                                files: [{
-                                        expand: true,
-                                        cwd: 'scanner/',
-                                        src: ['**/*.{jpg,gif,png}'],
-                                        custom_dest: 'tmp/images/{%= name %}/'
-                                }]
-                        }
-                },
+  grunt.initConfig({
+    responsive_images: {
+      myTask: {
+        options: {
+          sizes: [{
+            name: "small",
+            width: 160,
+            height: 120
+          },{
+            name: 'medium',
+            width: 320,
+            height: 240
+          },{
+            name: 'large',
+            width: 640,
+            height: 480
+          }]
+        },
+        files: [{
+          expand: true,
+          cwd: 'scanner/',
+          src: ['**/*.{jpg,gif,png}'],
+          custom_dest: 'tmp/images/{%= name %}/'
+        }]
+      }
+    },
 
-                imagemin: {
-                        dynamic: {
-                                options: {
-                                        optimizationLevel: 3,
-                                        progressive: true,
-                                        interlaced: true
-                                },
-                                files: [{
-                                        expand: true,
-                                        cwd: 'tmp/images/',
-                                        src: ['**/*.{png,jpg,jpeg,gif}'],
-                                        dest: 'images/'
-                                }]
-                        }
-                }    
-        });
+    imagemin: {
+      dynamic: {
+        options: {
+          optimizationLevel: 3,
+          progressive: true,
+          interlaced: true
+        },
+        files: [{
+          expand: true,
+          cwd: 'tmp/images/',
+          src: ['**/*.{png,jpg,jpeg,gif}'],
+          dest: 'images/'
+        }]
+      }
+    },
 
-        grunt.loadNpmTasks('grunt-contrib-imagemin');
-        grunt.loadNpmTasks('grunt-responsive-images');
-        
-        grunt.registerTask('resize', ['responsive_images']);
-        grunt.registerTask('min', ['imagemin']);
+    sass: {
+      dist: {
+        options: {
+          style: 'expanded'
+        },
+        files: [{
+          expand: true,
+          cwd: 'styles',
+          src: 'main.scss',
+          dest: 'css',
+          ext: '.css'
+        }]
+      }
+    }
+  });
 
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+
+  grunt.registerTask('resize', ['responsive_images']);
+  grunt.registerTask('min', ['imagemin']);
+  grunt.registerTask('default', ['sass']);
 
 };
